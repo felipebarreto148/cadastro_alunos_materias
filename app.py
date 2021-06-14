3bee677059108c2fb82d314856d55a8bb1aac899
felipe barreto <felipebarreto148@gmail.com>
2021-06-14 20:11:39 -0300

    Funcionalidade de adicionar novos alunos na tabela criada

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from tkinter import Frame, ttk
+from tkinter import ttk
 from tkinter import *
+import tkinter.messagebox as msb
+import sqlite3
 import bdalunos as bdA
 
 # --------- Variables -----------
@@ -8,83 +10,143 @@ app = tk.Tk()
 app.title('Cadastro de alunos e notas')
 app.resizable(False, False)
 
-app.geometry('1000x500')
+app.geometry('700x400')
 app.config(bg='#167ec4')
+
+def inserirData():
+    pass
 
 
 # ----- VARIAVEIS ALUNO -----
 nome = StringVar()
 idade = StringVar()
-turma = StringVar()
+email = StringVar()
+telefone = StringVar()
 
-# ----- Telas -----
+# ----- METODOS -----
+def submitData():
+    if nome.get() == "" or telefone.get() == "" or idade.get() == "" or email.get() == "":
+        resultado = msb.showwarning("", "Por favor, digite todos os campos.", icon="warning")
+    else:
+        tableAlunos.delete(*tableAlunos.get_children())
+        conn = sqlite3.connect("./cadastro.db")
+        cursor = conn.cursor()
+        query = """ INSERT INTO 'alunos' (nome, telefone, email, idade) VALUES (?, ?, ?, ?)"""
+        cursor.execute(query, (str(nome.get()), str(telefone.get()), 
+                        str(email.get()), str(idade.get())))
+        conn.commit()
+        cursor.execute('SELECT * FROM alunos ORDER BY nome')
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tableAlunos.insert('', 'end', values=(data))
+        cursor.close()
+        conn.close()
+        nome.set("")
+        telefone.set("")
+        idade.set("")
+        email.set("")
+
+
 def addAluno():
     newWin = tk.Toplevel()
     newWin.title('Cadastrar Aluno')
     newWin.geometry('400x300')
-    newWin.config(bg='#bbffbb')
+    newWin.config(bg='#119922')
+    newWin.resizable(0, 0)
 
-def addMateria():
-    newWin = tk.Toplevel()
-    newWin.title('Cadastrar Matéria')
-    newWin.geometry('400x300')
-    newWin.config(bg='#bbffbb')
+    # --------- FRAME DO INCLUDE ----------
+    formTitle = Frame(newWin)
+    formTitle.pack(side=TOP)
+    formContact = Frame(newWin, bg="#119922")
+    formContact.pack(side=TOP, pady=10)
+    # --------- LABEL DO INCLUDE ----------
+    Label(formTitle, text="Inserindo Aluno", font=('arial', 18), fg= '#ffffff', bg='#119922').pack(fill=X)
+    Label(formContact, text="Nome", font=('arial', 12), bg='#119922', fg='#ffffff').grid(row=0, sticky=W, pady=8)
+    Label(formContact, text="Telefone", font=('arial', 12), bg='#119922', fg='#ffffff').grid(row=1, sticky=W, pady=8)
+    Label(formContact, text="Idade", font=('arial', 12), bg='#119922', fg='#ffffff').grid(row=2, sticky=W, pady=8)
+    Label(formContact, text="Email", font=('arial', 12), bg='#119922', fg='#ffffff').grid(row=3, sticky=W, pady=8)
 
-# ------ Buttons Aluno -------
-tk.Button(app, text="Incluir Aluno", bg="#009900", font=("Arial"), fg="#ffffff", command=addAluno).grid(row=0, column=0, padx=8, pady=8)
-tk.Button(app, text="Remover Aluno", bg="#bb0000", font=("Arial"), fg="#ffffff").grid(row=0, column=1, padx=8, pady=8)
+    # --------- ENTRY DO INCLUDE ----------
+    Entry(formContact, textvariable=nome, font=('arial', 12)).grid(row=0, column=1)
+    Entry(formContact, textvariable=telefone, font=('arial', 12)).grid(row=1, column=1)
+    Entry(formContact, textvariable=idade, font=('arial', 12)).grid(row=2, column=1)
+    Entry(formContact, textvariable=email, font=('arial', 12)).grid(row=3, column=1)
 
-# ------ Label ------
-tk.Label(app, text="Bem vindo", bg='#167ec4', font=("Arial", 20), fg="#ffffff").grid(row=0, column=2, padx=10)
-tk.Label(app, text="Alunos", bg='#167ec4', font=("Arial", 20), fg="#ffffff").grid(row=1, column=0, columnspan=2)
-tk.Label(app, text="Notas", bg='#167ec4', font=("Arial", 20), fg="#ffffff").grid(row=1, column=3, columnspan=2)
+    # --------- BUTTON DO INCLUDE ---------
+    Button(formContact, text="Inserir", font=('Arial', 18), command=submitData).grid(row=6, columnspan=2, pady=10)
 
-# ------ Buttons Notas ----
-# tk.Button(app, text="Incluir Matéria", bg="#009900", font=("Arial"), fg="#ffffff", command=addMateria).grid(row=0, column=3, padx=8, pady=8)
-# tk.Button(app, text="Remover Matéria", bg="#bb0000", font=("Arial"), fg="#ffffff").grid(row=0, column=4, padx=8, pady=8)
+
+# # ------ Buttons Aluno -------
+tk.Button(app, text="Incluir Aluno", bg="#009900", font=("Arial"), fg="#ffffff", command=addAluno).grid(row=0, column=0, padx=8)
+tk.Button(app, text="Remover Aluno", bg="#bb0000", font=("Arial"), fg="#ffffff").grid(row=1, column=0, padx=8, pady=8)
 
 
 # ------ Tabelas ----
     # ---- Columns -----
-columnsA = ("Matricula", "Nome", "Idade", "Turma")
-columnsN = ("Materia", "AV1", "AV2", "AV3", "AVD", "AVDS", "Media")
+columnsA = ("Matricula", "Nome", "Idade", "Email", "Telefone" )
+#columnsN = ("Materia", "AV1", "AV2", "AV3", "AVD", "AVDS", "Media")
 
-    #----- Table Alunos -----
+#     #----- Table Alunos -----
 tableAlunos = ttk.Treeview(app, columns=columnsA, show="headings")
 
 tableAlunos.heading("Matricula", text="ID", anchor=W)
 tableAlunos.heading("Nome", text="Nome", anchor=W)
 tableAlunos.heading("Idade", text="Idade", anchor=W)
-tableAlunos.heading("Turma", text="Turma", anchor=W)
+tableAlunos.heading("Email", text="Email", anchor=W)
+tableAlunos.heading("Telefone", text="Telefone", anchor=W)
 
-tableAlunos.column('#1', stretch=NO, minwidth=0, width=30)
-tableAlunos.column('#2', stretch=NO, minwidth=0, width=150)
+tableAlunos.column('#1', stretch=NO, minwidth=0, width=50)
+tableAlunos.column('#2', stretch=NO, minwidth=0, width=100)
 tableAlunos.column('#3', stretch=NO, minwidth=0, width=50)
-tableAlunos.column('#4', stretch=NO, minwidth=0, width=50)
+tableAlunos.column('#4', stretch=NO, minwidth=0, width=150)
+tableAlunos.column('#5', stretch=NO, minwidth=0, width=100)
 
-tableAlunos.grid(row=2, column=0, columnspan=2)
+tableAlunos.grid(row=0, column=2, padx=(8, 0), pady=8, rowspan=2)
 
     #----- Table Notas -----
-tableNotas = ttk.Treeview(app, columns=columnsN, show="headings")
+# tableNotas = ttk.Treeview(app, columns=columnsN, show="headings")
 
-tableNotas.heading("Materia", text="Matéria", anchor=W)
-tableNotas.heading("AV1", text="AV1", anchor=W)
-tableNotas.heading("AV2", text="AV2", anchor=W)
-tableNotas.heading("AV3", text="AV3", anchor=W)
-tableNotas.heading("AVD", text="AVD", anchor=W)
-tableNotas.heading("AVDS", text="AVDS", anchor=W)
-tableNotas.heading("Media", text="Media", anchor=W)
+# tableNotas.heading("Materia", text="Matéria", anchor=W)
+# tableNotas.heading("AV1", text="AV1", anchor=W)
+# tableNotas.heading("AV2", text="AV2", anchor=W)
+# tableNotas.heading("AV3", text="AV3", anchor=W)
+# tableNotas.heading("AVD", text="AVD", anchor=W)
+# tableNotas.heading("AVDS", text="AVDS", anchor=W)
+# tableNotas.heading("Media", text="Media", anchor=W)
 
-tableNotas.column('#1', stretch=NO, minwidth=0, width=100)
-tableNotas.column('#2', stretch=NO, minwidth=0, width=50)
-tableNotas.column('#3', stretch=NO, minwidth=0, width=50)
-tableNotas.column('#4', stretch=NO, minwidth=0, width=50)
-tableNotas.column('#5', stretch=NO, minwidth=0, width=50)
-tableNotas.column('#6', stretch=NO, minwidth=0, width=50)
+# tableNotas.column('#1', stretch=NO, minwidth=0, width=100)
+# tableNotas.column('#2', stretch=NO, minwidth=0, width=50)
+# tableNotas.column('#3', stretch=NO, minwidth=0, width=50)
+# tableNotas.column('#4', stretch=NO, minwidth=0, width=50)
+# tableNotas.column('#5', stretch=NO, minwidth=0, width=50)
+# tableNotas.column('#6', stretch=NO, minwidth=0, width=50)
 
-tableNotas.grid(row=2, column=3, columnspan=2)
+# tableNotas.grid(row=2, column=3, columnspan=2)
+
+
+# ------------------ Criação do Menu ------------------
+menu = Menu(app)
+app.config(menu=menu)
+
+# Menu Aluno
+fileMenu = Menu(menu, tearoff = 0)
+menu.add_cascade(label="Alunos", menu=fileMenu)
+fileMenu.add_command(label="Criar Novo", command=addAluno)
+fileMenu.add_command(label="Editar", command=inserirData)
+fileMenu.add_command(label="Deletar", command=inserirData)
+fileMenu.add_separator()
+fileMenu.add_command(label="Sair", command=app.destroy)
+
+# Menu Matéria
+fileMenu = Menu(menu, tearoff = 0)
+menu.add_cascade(label="Matérias", menu=fileMenu)
+fileMenu.add_command(label="Criar Nova", command=inserirData)
+fileMenu.add_command(label="Editar", command=inserirData)
+fileMenu.add_command(label="Remover", command=inserirData)
+fileMenu.add_separator()
+fileMenu.add_command(label="Sair", command=app.destroy)
 
 
 if __name__ == "__main__":
-    #bdA.connect()
+    bdA.connect()
     app.mainloop()
--- bdalunos.py
+++ bdalunos.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('./trabalhoAV2/escola.db')
+conn = sqlite3.connect('./cadastro.db')
 cursor = conn.cursor()
 
 
@@ -9,15 +9,18 @@ def connect():
         matricula INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
         nome TEXT NOT NULL,
         idade INTEGER NOT NULL,
-        turma TEXT NOT NULL,
+        email TEXT NOT NULL,
+        telefone TEXT NOT NULL
     ) """
-    conn.execute(query)
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
 
 
-def insert(nome, idade, turma):
+def insert(nome, idade, email, telefone):
     try:
-        query = 'INSERT INTO alunos (nome, email, telefone, idade, turma, endereco) VALUES (?, ?, ?, ?, ?, ?)', (
-            nome, email, telefone, idade, turma, endereco)
+        query = 'INSERT INTO alunos (nome, idade, email, telefone) VALUES (?, ?, ?, ?, ?, ?)', (
+            nome, idade, email, telefone)
         conn.execute(query)
         conn.commit()
     except sqlite3.Error as err:
@@ -32,19 +35,19 @@ def consult():
         return 'Erro ao consultar as alunos: ', err
 
 
-def update(id_aluno, nome, idade, turma):
+def update(matricula, nome, idade, email, telefone):
     try:
-        query = 'UPDATE alunos SET nome = ?, email = ?, telefone = ?, idade = ?, turma = ?, endereco = ? WHERE id_aluno = ?', (
-            nome, email, telefone, idade, turma, endereco, id_aluno)
+        query = 'UPDATE alunos SET nome = ?, email = ?, telefone = ?, idade = ? WHERE matricula = ?', (
+            nome, email, telefone, idade, matricula)
         cursor.execute(query)
         conn.commit()
     except sqlite3.Error as err:
         return 'Erro ao alterar uma matéria: ', err
 
 
-def delete(id_aluno):
+def delete(matricula):
     try:
-        query = 'DELETE FROM alunos WHERE id_aluno = ?', (id_aluno)
+        query = 'DELETE FROM alunos WHERE matricula = ?', (matricula)
         conn.execute(query)
         conn.commit()
     except sqlite3.Error as err:
