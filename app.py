1050cb8e20698fab5983461592642f26758a1837
Felipe Barreto <felipebarreto148@gmail.com>
2021-06-14 09:44:46 -0300

    Tela inicial criada com os botoes de inclusao e remoçao de alunos e materias

---
+++ app.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import font
+
+# --------- Variables -----------
+app = tk.Tk()
+app.title('Cadastro de alunos e notas')
+app.resizable(True, False)
+
+app.geometry('800x600')
+app.config(bg='#ffab65')
+
+# ------ Buttons Aluno -------
+tk.Button(app, text="Incluir Aluno", bg="#009900", font=("Arial"), fg="#ffffff").grid(row=0, column=0, padx=8, pady=8)
+tk.Button(app, text="Remover Aluno", bg="#bb0000", font=("Arial"), fg="#ffffff").grid(row=0, column=1, padx=8, pady=8)
+
+# ------ Label ------
+tk.Label(app, text="Bem vindo", bg='#ffab65', font=("Arial", 20)).grid(row=0, column=2, padx=35)
+
+# ------ Buttons Matérias ----
+tk.Button(app, text="Incluir Matéria", bg="#009900", font=("Arial"), fg="#ffffff").grid(row=0, column=3, padx=8, pady=8)
+tk.Button(app, text="Remover Matéria", bg="#bb0000", font=("Arial"), fg="#ffffff").grid(row=0, column=4, padx=8, pady=8)
+
+
+if __name__ == "__main__":
+    app.mainloop()
