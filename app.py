af58a2ef3934e53ae34f025334b8dae1b1897d05
Felipe Barreto <felipebarreto148@gmail.com>
2021-06-14 12:01:08 -0300

    Adicionado titulo das tabelas de aluno e materia

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ tk.Button(app, text="Remover Aluno", bg="#bb0000", font=("Arial"), fg="#ffffff")
 
 # ------ Label ------
 tk.Label(app, text="Bem vindo", bg='#ffab65', font=("Arial", 20)).grid(row=0, column=2, padx=35)
+tk.Label(app, text="Alunos", bg='#ffab65', font=("Arial", 20)).grid(row=1, column=1)
+tk.Label(app, text="Matérias", bg='#ffab65', font=("Arial", 20)).grid(row=1, column=3)
+
 
 # ------ Buttons Matérias ----
 tk.Button(app, text="Incluir Matéria", bg="#009900", font=("Arial"), fg="#ffffff").grid(row=0, column=3, padx=8, pady=8)
